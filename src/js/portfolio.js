8dacb80290f665d9c48763c15c0b201e073e8e9b
Jacob King <jacobuid@users.noreply.github.com>
2017-03-17 17:44:09 -0500

    Update portfolio.js

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,5 +1,13 @@
 console.info("portfolio.js is loaded...");
 
 var run = function(){
-    console.info("run() ran...");
+	console.info("run() ran...");
+
+	$.ajax({
+		url: "https://api.linkedin.com/v1/people/~?format=json",
+		method: "GET"
+	}).done(function(response){
+		console.log(response)
+	});
+
 }
