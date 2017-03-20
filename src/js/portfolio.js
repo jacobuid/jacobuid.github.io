c5716a77c6c6483c45b8d1ff7d912c2f00df57d1
Jacob King <jacobuid@users.noreply.github.com>
2017-03-19 22:23:03 -0500

    Update portfolio.js

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,12 +1,10 @@
 console.info("portfolio.js is loaded...");
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 	$.getJSON("https://api.twitter.com/1/users/show.json?callback=?&amp;screen_name=jacobuid", function (data) {
-           console.log(data);
-        });
-    });
-
+	   console.log(data);
+	});
 });
 
 
